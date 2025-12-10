b99de6d969773eaabf75684782592882043f26e9
ZhaoShurui728 <g2199192503@gmail.com>
2025-12-10 14:50:29 +0900

    new csv

--- PY/tools/gdxtocsv.py
+++ PY/tools/gdxtocsv.py
@@ -4,10 +4,10 @@ import pandas as pd
 # ---------- 第一次运行：读取 GDX ----------
 gdx = gdxpds.to_dataframes("./GDX/data_prep.gdx")
 
-df = gdx["MAP_RISOG"].copy()
-df.columns = ["RISO","G", "Text"]
+df = gdx["GAIJ"].copy()
+df.columns = ["I","J", "Value"]
 
 # 保存为 CSV（这是关键）
-df.to_csv("./CSV/RISOG.csv", index=False)
+df.to_csv("./CSV/GAIJ.csv", index=False)
 
 print("第一次处理完成，已保存csv！以后都不需要再读GDX。")
