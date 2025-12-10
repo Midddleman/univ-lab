eb42a374cc7937c70d767b842597db2c070afdc8
ZhaoShurui728 <g2199192503@gmail.com>
2025-12-10 12:55:59 +0900

    new csv

--- PY/tools/gdxtocsv.py
+++ PY/tools/gdxtocsv.py
@@ -2,12 +2,12 @@ import gdxpds
 import pandas as pd
 
 # ---------- 第一次运行：读取 GDX ----------
-gdx = gdxpds.to_dataframes("./GDX/region analysis.gdx")
+gdx = gdxpds.to_dataframes("./GDX/data_prep.gdx")
 
-df = gdx["Area"].copy()
-df.columns = ["region", "year", "type", "value"]
+df = gdx["MAP_RISOG"].copy()
+df.columns = ["RISO","G", "Text"]
 
 # 保存为 CSV（这是关键）
-df.to_csv("./CSV/region aggregated.csv", index=False)
+df.to_csv("./CSV/RISOG.csv", index=False)
 
 print("第一次处理完成，已保存csv！以后都不需要再读GDX。")
