3745081b7a96783f1b42d9a58837c3a2a4cabbbe
ZhaoShurui728 <g2199192503@gmail.com>
2025-12-12 21:42:50 +0900

    prog update

--- PY/tools/cut_2_3f.py
+++ PY/tools/cut_2_3f.py
@@ -1,9 +1,13 @@
 import pandas as pd
+names = ["agri","grassland","forest"] 
+modes = ["basin","region"]
 
-df = pd.read_csv("./CSV/moran/world_moran.csv")
+variables = [f"{m}_{n}" for n in names for m in modes]
+for v in variables:
+    df = pd.read_csv(f"./CSV/moran/{v}_moran.csv")
 
 
-num_cols = df.columns[1:]
-df[num_cols] = df[num_cols].applymap(lambda x: f"{x:.3f}" if isinstance(x, (int, float)) else x)
+    num_cols = df.columns[1:]
+    df[num_cols] = df[num_cols].applymap(lambda x: f"{x:.3f}" if isinstance(x, (int, float)) else x)
 
-df.to_csv("./CSV/moran/world_moran_3f.csv", index=False)
+    df.to_csv(f"./CSV/moran/{v}_moran.csv_3f.csv", index=False)
