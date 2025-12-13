b1505dc5e166e20e263a37587c2907106108777f
ZhaoShurui728 <g2199192503@gmail.com>
2025-12-13 20:50:25 +0900

    basin area csv

--- PY/tools/gdx_2_csv.py
+++ PY/tools/gdx_2_csv.py
@@ -2,12 +2,12 @@ import gdxpds
 import pandas as pd
 
 # ---------- 第一次运行：读取 GDX ----------
-gdx = gdxpds.to_dataframes("./GDX/data_prep.gdx")
+gdx = gdxpds.to_dataframes("./GDX/data.gdx")
 
-df = gdx["GAIJ"].copy()
-df.columns = ["I","J", "Value"]
+df = gdx["LUH2"].copy()
+df.columns = ["basin","city","land","year" ,"Value"]
 
 # 保存为 CSV（这是关键）
-df.to_csv("./CSV/GAIJ.csv", index=False)
+df.to_csv("./CSV/Basin_area.csv", index=False)
 
 print("第一次处理完成，已保存csv！以后都不需要再读GDX。")
