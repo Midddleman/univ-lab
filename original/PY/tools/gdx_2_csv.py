b5db80aa3766a41818550f8eb09204f58d2d61a2
ZhaoShurui728 <g2199192503@gmail.com>
2025-12-19 22:41:09 +0900

    aglu csv

--- original/PY/tools/gdx_2_csv.py
+++ original/PY/tools/gdx_2_csv.py
@@ -2,12 +2,12 @@ import gdxpds
 import pandas as pd
 
 # ---------- 第一次运行：读取 GDX ----------
-gdx = gdxpds.to_dataframes("./GDX/base_region.gdx")
+gdx = gdxpds.to_dataframes("./GDX/SSP2_BaU_NoCC_World.gdx")
 
-df = gdx["YIELDLDM_annual"].copy()
-df.columns = ["basin","year" ,"land","Value"]
+df = gdx["Yld2_load"].copy()
+df.columns = ["category","basin","country" ,"year","Value"]
 
 # 保存为 CSV（这是关键）
-df.to_csv("./original/CSV/IAMC/Region_yield.csv", index=False)
+df.to_csv("./original/CSV/aglu/yield.csv", index=False)
 
 print("第一次处理完成，已保存csv！以后都不需要再读GDX。")
